407a3de7da0cf3340828d9adce2119212c4b864f
Skobachke <skobachke@gmail.com>
2017-03-12 20:19:39 +0300

    Первая версия программы. Readme

--- pprint_json.py
+++ pprint_json.py
@@ -2,14 +2,14 @@ import json
 
 
 def load_data(filepath):
-    with open(filepath, 'r') as js:  # открываем файл на чтение
+    with open(filepath, 'rt') as js:  # открываем файл на чтение
         python_data = json.load(js) # загружаем из файла данные в словарь
         js.close()  # Закрываем файл
         return python_data
 
 
 def pretty_print_json(data):
-    print(json.dumps(data, sort_keys=True, indent=4))
+    print (json.dumps(data, ensure_ascii=False, sort_keys=True, indent=4))
 
 
 if __name__ == '__main__':
